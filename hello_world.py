d40e339fd9986e590f53a2d1dbab4e559b4b5cf2
aliwenying <work_yhy@hotmail.com>
2017-11-02 18:43:24 +0800

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,8 +1,4 @@
-name = "ada lovElace";
-print(name);
-#首字母改大写
-print(name.title());
-#字符串全部改为大写
-print(name.upper());
-#字符串全部改为小写
-print(name.lower());
+message="hello python!";
+print(message);
+message="hello python! line two";
+print(message);
